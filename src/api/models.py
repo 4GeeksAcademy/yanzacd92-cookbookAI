1bbfcabada895ff2e8273b3c8d425238407a9115
yanzacd92 <121258474+yanzacd92@users.noreply.github.com>
2023-05-25 17:24:07 +0000

    Added views and models User, Category, Recipe and Favorite

--- src/api/models.py
+++ src/api/models.py
@@ -7,6 +7,8 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    security_question = db.Column(db.String(100), unique=False, nullable=False)
+    security_answer = db.Column(db.String(150), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -14,6 +16,72 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "email": self.email
+            # do not serialize the password, its a security breach
+        }
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(200), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<Category {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
             "email": self.email,
+            "description": self.description
+            # do not serialize the password, its a security breach
+        }
+
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(200), unique=False, nullable=False)
+    elaboration = db.Column(db.String(500), unique=False)
+    image = db.Column(db.String(500), unique=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship(Category)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
+
+
+    def __repr__(self):
+        return f'<Recipe {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "description": self.description,
+            "image": self.image,
+            "elaboration": self.elaboration
+            # do not serialize the password, its a security breach
+        }
+
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    security_question = db.Column(db.String(100), unique=False, nullable=False)
+    security_answer = db.Column(db.String(150), unique=False, nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'))
+    recipe = db.relationship(Recipe)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email
             # do not serialize the password, its a security breach
         }
