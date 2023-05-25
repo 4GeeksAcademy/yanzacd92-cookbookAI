42c0863086f05c6d8b8985f6a743304bf8f4abb0
yanzacd92 <121258474+yanzacd92@users.noreply.github.com>
2023-05-25 14:12:11 +0000

    Added login backend

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,27 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_bcrypt import Bcrypt
 
 api = Blueprint('api', __name__)
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
-
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+@api.route('/login', methods=['POST'])
+def user_login():
+    user_username = request.json.get("username")
+    user_password = request.json.get("password")
+    user = User.query.filter_by(username=user_username).first()
+    if(user is None):
+        return jsonify({
+            "message": "User not found"
+        }), 401
+    # verify password
+    if not bcrypt.check_password_hash(user.password, user_password):
+        return jsonify({"message": "Wrong password"}), 401
+    
+    # generate token
+    access_token = create_access_token(identity = user.id)
+    return jsonify({"accessToken": access_token})
