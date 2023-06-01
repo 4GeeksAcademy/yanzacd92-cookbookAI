7487486574a1240c0de42a0e3fb5e661c608d0c0
yanzacd92 <121258474+yanzacd92@users.noreply.github.com>
2023-06-01 02:57:48 +0000

    added layouts

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -28,25 +28,13 @@ const getState = ({ getStore, getActions, setStore }) => {
 				return resp
 			},
 			userSignup: async(email, password) => {
-				const resp = await getActions().apiFetch("/api/signup", "POST", {email, password})
+				const resp = await getActions().apiFetch("/api/signup", "POST", {'headers': { 'Access-Control-Allow-Origin': '*'},}, {email, password})
 				if(resp.code >= 400) {
 					return resp
 				}
 				//setStore({accessToken: resp.data.accessToken})
 				localStorage.setItem("accessToken", resp.data.accessToken)
 				return resp
-			},
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await getActions().apiFetch("/hello")
-					const data = await resp.json
-					setStore({ message: resp.data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
 			},
 			apiFetch: async(endpoint, method="GET", body={}) => {
 				let response = await fetch(apiURL + endpoint, method == "GET" ? undefined: {
