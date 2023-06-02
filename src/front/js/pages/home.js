8c60021854c8ec8e86ee9e8a460cb66cfa18362a
yanzacd92 <121258474+yanzacd92@users.noreply.github.com>
2023-06-02 16:16:03 +0000

    added styles in login

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -20,27 +20,33 @@ export const Home = () => {
 		navigate("/api/signup");
 	}
 
+	function passwordRecovery() {
+		navigate("/api/passwordRecovery");
+	}
+
 	return (
-		<div className="container-login text-center mt-5">
+		<div className="container-login text-center">
 			<form className="login-form" onSubmit={submitForm}>
 				<h1 className="login-title">Login</h1>
 				<div className="mb-3">
 					<label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
 					<input type="email" className="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" />
-					<div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
 				</div>
 				<div className="mb-3">
 					<label htmlFor="exampleInputPassword1" className="form-label">Password</label>
 					<input type="password" className="form-control" name="password" id="exampleInputPassword1" />
 				</div>
+				<div className="password-recovery">
+					<button onClick={passwordRecovery} className="password-recovery-btn btn" type="button">Forgot Password?</button>
+				</div>
 				<div className="login">
 					<button type="submit" className="login-button btn btn-primary">Login</button>
 				</div>
+				<div className="signup-login">
+					<span className="mx-2">Not a member?</span>
+					<button onClick={signup} className="signup-btn btn" type="button">Signup</button>
+				</div>
 			</form>
-			<div className="signup-login">
-				<span className="mx-2">Not a member?</span>
-            	<button onClick={signup} className="signup-btn btn" type="button">Signup</button>
-        	</div>
 		</div>
 	);
 };
