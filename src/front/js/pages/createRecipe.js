a7d81e75db19aa48f0513315ab1f58eb712160e0
yanzacd92 <yanzacd@gmail.com>
2023-06-20 22:19:51 +0000

    added confirmation modal

--- src/front/js/pages/createRecipe.js
+++ src/front/js/pages/createRecipe.js
@@ -24,55 +24,48 @@ export const CreateRecipe = () => {
 
   async function createRecipe(e) {
     e.preventDefault()
-    //const formData = new FormData(e.target)
-    //const formFields = document.getElementById('form-create-recipe-id').elements;
+    const formData = new FormData(e.target)
+    const formFields = document.getElementById('form-create-recipe-id').elements;
     let resp_imagen = ""
 
     let new_recipe = await actions.userCreateRecipe(recommendedname, description, ingredient, instructions, resp_imagen)
     if(new_recipe.code >= 400) {
       swal("Opps!", "Recipe was not created", "error");
       navigate("/createRecipe")
-    } else{
-      swal("Amazing!", "Recipe has been created", "success");
+    } /*else{
+      swal("Congratulations!", "You have created your recipe", "success");
       navigate("/recipeDetail/" + new_recipe.data.id)
-    }
-    /*let recipeId = 0
+    }*/
+    let recipeId = 0
     Object.keys(new_recipe).map((key) => {
       if(key == "data")
         Object.keys(new_recipe[key]).map((k) => {
         if(k == "id") recipeId = new_recipe[key][k]
       })
-    })*/
+    })
+    console.log("RECIPE IMAGE    " + formFields['recipePicture'])
     // validate if user uploaded an image
-    /*if (formFields['recipePicture'].files.length > 0) {
+    if (formFields['recipePicture'].files.length > 0) {
       resp_firebase = await actions.uploadRecipePicture(formData, recipeId)
       if(resp_firebase.code >= 400) {
-        swal("Opps!", "Recipe was not created", "error");
-        //navigate("/createRecipe")
+        swal("Opps!", "The image has problems, do not worry!!! we have created your recipe but without any picture", "warning");
+        navigate("/recipeDetail/" + new_recipe.data.id)
       } else {
-      Object.keys(resp_firebase).map((key) => {
+        Object.keys(resp_firebase).map((key) => {
           if(key == "data") resp_imagen = (resp_firebase[key].filename)
-      }) }
-    }*/
-
-    // create image from URL
-    /*let formD = createImageFromUrl(recipePicture)
-    console.log("FORMDATA  " + formD)
-    console.log("URL IMAGE  " + recipePicture)
-    let resp_firebase = await actions.uploadRecipePicture(formD, recipeId)
-    Object.keys(resp_firebase).map((key) => {
-      if(key == "data") resp_imagen = (resp_firebase[key].filename)
-  })*/
+        })
+      }
+    }
     //console.log("FIRABASE IMAGE  " + resp_imagen)
-    //let resp_edit_recipe = await actions.userEditRecipe(recipeId, recommendedname, description, ingredient, instructions, resp_imagen)
-    /*setRecipePicture(resp_imagen)
+    let resp_edit_recipe = await actions.userEditRecipe(recipeId, recommendedname, description, ingredient, instructions, resp_imagen)
+    setRecipePicture(resp_imagen)
     if(resp_edit_recipe.code >= 400) {
       swal("Opps!", "Recipe was not created", "error");
+      navigate("/createRecipe")
     } else {
-      swal("Amazing!", "Recipe has been created", "success");
-      navigate("/recipeDetail/" + recipeId)
+      swal("Congratulations!", "You have created your recipe", "success");
+      navigate("/recipeDetail/" + new_recipe.data.id)
     }
-    swal("Amazing!", "Recipe has been created", "success");*/
   }
 
   async function callChatGPT() {
@@ -144,24 +137,6 @@ export const CreateRecipe = () => {
     return text
   }
 
-  function createImageFromUrl(imageUrl) {
-    //const imageUrl = 'https://example.com/image.jpg'; // Replace with your image URL
-    // Create a new FormData object
-    const formData = new FormData();
-    // Fetch the image file
-    fetch(imageUrl)
-      .then((response) => response.blob())
-      .then((blob) => {
-        // Append the image file to the FormData object
-        formData.append('image', blob, 'imagetest.jpg');
-      })
-      .catch((error) => {
-        // Handle any errors
-      });
-
-      return formData;
-  }
-
   return (
     <div>
       <Navbar createRecipe={"active"}/>
@@ -194,6 +169,12 @@ export const CreateRecipe = () => {
                 placeholder="Ingredient 1, ingredient 2, ..."
                 onChange={(e) => setIngredient(e.target.value)}
               />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="formFile" className="label-add-recipe form-label">
+                Image for recipe (<strong>optional</strong>)
+              </label>
+              <input className="form-control" type="file" name="recipePicture" id="formFile" />
             </div>
             <div className="col-12">
               <button
